a128477cbe3f812c6d9a95e8b4e52ac9fffaa7a2
Spam <GamerSpam@users.noreply.github.com>
2023-08-24 14:34:14 -0700

    New lesson added

---
+++ Small_Modules/grocery.py
@@ -0,0 +1,6 @@
+# Grocery program from Codedex
+
+grocery = ['🥚 Eggs', '🥑 Avocados', '🍪 Cookies', '🌶️  Hot Pepper Jam', '🫐  Blueberries', '🥦 Broccoli']
+
+for item in grocery: # Thanks to @max in the discord for providing this code snippet
+    print(item)
