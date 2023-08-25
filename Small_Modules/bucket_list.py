9d3abf61cbe2bbeda9b1da099e07117d17537e1b
Spam <GamerSpam@users.noreply.github.com>
2023-08-25 15:04:33 -0700

    Had to split into two commits

---
+++ Small_Modules/bucket_list.py
@@ -0,0 +1,13 @@
+# Bucket list lesson from Codedex
+
+things_to_do = [
+    'Buy my own house',
+    'reach 200K subs on twitch',
+    'Have a family',
+    'Discover myself',
+    'Learn at least 4 coding languages',
+    'Become a software engineer'
+]
+
+for i in things_to_do:
+    print(i)
---
+++ Small_Modules/mixtape.py
@@ -0,0 +1,23 @@
+# Mixtape lesson from Codedex
+
+# I'm using the list from Codedex
+# 💿 Theme: Indie Travel Songs
+
+print('\nMethod One\n')
+
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVP UP - Spirit Was',
+  'Mitski - First Love / Late Spring',
+]
+
+for i in playlist:
+    print(i)
+
+print('\nMethod Two\n')
+
+for s in range(len(playlist)):
+    print(playlist[s])
